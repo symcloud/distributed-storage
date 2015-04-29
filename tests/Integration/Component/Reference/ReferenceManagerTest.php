55748b091ac12c5435dd27964698b63aa5618023
Johannes Wachter <johannes.wachter@massiveart.at>
2015-04-29 16:52:10 +0200

    added testcases

---
+++ tests/Integration/Component/Reference/ReferenceManagerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Integration\Component\Reference;
+
+use Prophecy\PhpUnit\ProphecyTestCase;
+
+class ReferenceManagerTest extends ProphecyTestCase
+{
+    public function testGetForUserReference()
+    {
+        $this->markTestIncomplete('This test is not implemented until now');
+    }
+
+    public function testUpdateReference()
+    {
+        $this->markTestIncomplete('This test is not implemented until now');
+    }
+
+    public function testCreateReference()
+    {
+        $this->markTestIncomplete('This test is not implemented until now');
+    }
+}
