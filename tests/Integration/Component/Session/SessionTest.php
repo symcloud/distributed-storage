a87f2c264914c458d44fab58de3cc947e59ba0ae
Johannes Wachter <johannes.wachter@massiveart.at>
2015-05-06 20:17:25 +0200

    added upload tests

--- tests/Integration/Component/Session/SessionTest.php
+++ tests/Integration/Component/Session/SessionTest.php
@@ -2,7 +2,9 @@
 
 namespace Integration\Component\Session;
 
+use Basho\Riak\Bucket;
 use Integration\Parts\ReferenceManagerTrait;
+use Integration\Parts\TestFileTrait;
 use Prophecy\PhpUnit\ProphecyTestCase;
 use Symcloud\Component\MetadataStorage\Model\CommitInterface;
 use Symcloud\Component\MetadataStorage\Model\ReferenceInterface;
@@ -13,7 +15,7 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class SessionTest extends ProphecyTestCase
 {
-    use ReferenceManagerTrait;
+    use ReferenceManagerTrait, TestFileTrait;
 
     /**
      * @var mixed
@@ -54,15 +56,7 @@ class SessionTest extends ProphecyTestCase
         $this->assertEquals('/', $root->getPath());
         $this->assertEquals(array(), $root->getChildren());
 
-        $keys = $this->fetchBucketKeys($this->getMetadataBucket())->getObject()->getData()->keys;
-
-        $objects = array();
-        foreach ($keys as $key) {
-            $response = $this->fetchObject($key, $this->getMetadataBucket());
-            if ($response->isSuccess()) {
-                $objects[$key] = $response->getObject()->getData();
-            }
-        }
+        $objects =$this->getObjects($this->getMetadataBucket());
 
         $referenceKey = $username . '-' . $referenceName;
         $this->assertArrayHasKey($referenceKey, $objects);
@@ -88,6 +82,64 @@ class SessionTest extends ProphecyTestCase
             array(TreeInterface::TREE_TYPE => array(), TreeInterface::FILE_TYPE => array()),
             (array)$tree[TreeInterface::CHILDREN_KEY]
         );
+    }
+
+    public function testUpload()
+    {
+        $username = 'johannes';
+        $referenceName = 'HEAD';
+
+        list($fileContent, $fileName) = $this->generateTestFile(200);
+
+        $userMock = $this->prophesize(UserInterface::class);
+        $userMock->getUsername()->willReturn($username);
+        $user = $userMock->reveal();
+
+        $session = new Session(
+            $this->getBlobFileManager(),
+            $this->getReferenceManager(),
+            $this->getTreeManager(),
+            $this->getCommitManager(),
+            $referenceName,
+            $user
+        );
+
+        $blobFile = $session->upload($fileName);
+
+        $fileHash = $this->getFactory()->createFileHash($fileName);
+
+        $this->assertEquals($fileHash, $blobFile->getHash());
+        $this->assertEquals($this->getFactory()->createHash($fileContent), $blobFile->getHash());
+        $this->assertEquals($fileContent, $blobFile->getContent());
+
+        $blob1 = substr($fileContent, 0, 100);
+        $hash1 = $this->getFactory()->createHash($blob1);
+        $blob2 = substr($fileContent, 100, 100);
+        $hash2 = $this->getFactory()->createHash($blob2);
+
+        $blobFile = $this->fetchObject($fileHash, $this->getBlobFileBucket())->getObject()->getData();
+        $this->assertEquals(array($hash1, $hash2), $blobFile);
+
+        $object1 = $this->fetchObject($hash1, $this->getBlobBucket());
+        $object2 = $this->fetchObject($hash2, $this->getBlobBucket());
+
+        $this->assertEquals($blob1, $object1->getObject()->getData());
+        $this->assertEquals($blob2, $object2->getObject()->getData());
+    }
+
+    private function getObjects(Bucket $bucket)
+    {
+        $keys = $this->fetchBucketKeys($bucket)->getObject()->getData()->keys;
+
+        $objects = array();
+        foreach ($keys as $key) {
+            $response = $this->fetchObject($key, $this->getMetadataBucket());
+            if ($response->isSuccess()) {
+                $objects[$key] = $response->getObject()->getData();
+            }
+        }
+
+        return $objects;
     }
 
     protected function createUserProvider()
